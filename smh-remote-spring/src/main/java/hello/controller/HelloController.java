6ac2588339ea896651909dd077197e7730a42723
Waser Sascha <sascha.waser@mobi.ch>
2019-05-24 11:11:22 +0200

    added rest api base path

--- smh-remote-spring/src/main/java/hello/controller/HelloController.java
+++ smh-remote-spring/src/main/java/hello/controller/HelloController.java
@@ -2,14 +2,14 @@ package hello.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/foos")
+@RequestMapping("${spring.data.rest.base-path}/foos")
 public class HelloController {
 
     private Logger logger = LoggerFactory.getLogger(HelloController.class);
@@ -17,7 +17,7 @@ public class HelloController {
     @ApiOperation(value = "says hello",
             notes = "Returns Appointment Details Like Doctor's Name, Timing",
             response = String.class)
-    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    @GetMapping(value="/hello")
     public String hello() {
         logger.info("Hello info  is reached");
         logger.debug("Hello debug  is reached");
